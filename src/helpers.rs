c063c3c94ba664e2ed1804290b97437d7069d9dd
Vasyl Lukashuk <vas77794@gmail.com>
2023-03-16 14:55:24 +0100

    fix: return eprintln

--- src/helpers.rs
+++ src/helpers.rs
@@ -157,13 +157,13 @@ pub fn sanity() -> String {
                 let vol = val.strip_prefix("unix://").unwrap_or(&val).to_string();
                 format!(r#"-e DOCKER_HOST="{val}" -v "{vol}":"{vol}""#)
             } else if val.starts_with("tcp://") {
-                warn!("Docker socket with TCP schema was detected. Will use DOCKER_HOST={val} to access docker socket inside container.");
+                eprintln!("Docker socket with TCP schema was detected. Will use DOCKER_HOST={val} to access docker socket inside container.");
                 format!(r#"-e DOCKER_HOST="{val}""#)
             } else if val.starts_with(&docker0_ip) {
                 if !docker0_if_exist {
-                    warn!("Interface 'docker0' was not found but docker socket with TCP schema was detected. Will use default docker network 172.17.0.1.");
+                    eprintln!("Interface 'docker0' was not found but docker socket with TCP schema was detected. Will use default docker network 172.17.0.1.");
                 }
-                warn!("IP address was detected. Will use DOCKER_HOST=tcp://{docker0_ip} to access docker socket inside container.");
+                eprintln!("IP address was detected. Will use DOCKER_HOST=tcp://{docker0_ip} to access docker socket inside container.");
                 format!(r#"-e DOCKER_HOST="tcp://{docker0_ip}""#)
             } else {
                 eprintln!("Docker socket: {val} with unknown schema was detected.");
